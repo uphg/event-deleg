451ab5109889d79c30af4fe2aa4da5b24d12ccd6
chen <1834805770@qq.com>
2022-01-29 18:15:29 +0800

    实现事件委托的添加、删除

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,53 @@
+const DELEG = Symbol('delegateEvent')
+const LISTEN = Symbol('listenerState')
+
+interface ListenerCallback {
+  (e: Event, ...args: unknown[]): unknown
+  [DELEG]?: EventListenerOrEventListenerObject
+  [LISTEN]?: boolean | null
+}
+
 export function on(
   element: HTMLElement | Document | Window,
-  event: string,
-  handler: EventListenerOrEventListenerObject,
-  useCapture = false
+  eventType: string,
+  selector: string | ListenerCallback,
+  callback?: ListenerCallback
 ) {
-  element.addEventListener(event, handler, useCapture)
+  if (!element || !eventType || !selector) return
+  if (typeof selector === 'function') {
+    (selector as ListenerCallback)[LISTEN] = true
+    element.addEventListener(eventType, selector)
+  } else {
+    const listener =  (e: Event) => {
+      let el = e.target
+      while (!(el as Element)?.matches(selector)) {
+        if (element === el) {
+          el = null
+          break
+        }
+        el = (el as Element)?.parentNode
+      }
+      el && (callback as ListenerCallback).call(el, e, el)
+    }
+    if (callback) {
+      callback[DELEG] = listener
+      element.addEventListener(eventType, listener)
+    }
+  }
+  return element
 }
 
 export function off(
   element: HTMLElement | Document | Window,
-  event: string,
-  handler: EventListenerOrEventListenerObject,
-  useCapture = false
+  eventType: string,
+  callback: ListenerCallback
 ) {
-  element.removeEventListener(event, handler, useCapture)
+  if (!element || !eventType || !callback) return
+  if (callback[LISTEN]) {
+    callback[LISTEN] = null
+    element.removeEventListener(eventType, callback)
+  }
+  const deleg = callback[DELEG]
+  deleg && element.removeEventListener(eventType, deleg)
+  return element
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,69 @@
+import { off, on } from './index'
 import './style.css'
 
 const app = document.querySelector<HTMLDivElement>('#app')!
 
-app.innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+const createEl = (tagName: string) => {
+  return document.createElement(tagName)
+}
+
+{
+  console.log('事件监听')
+  const div = createEl('div')
+  div.style.marginBottom = '20px'
+  const btn1 = createEl('button')
+  btn1.textContent = '按钮1'
+
+  const btn2 = createEl('button')
+  btn2.textContent = '按钮1'
+
+  const clear1 = createEl('button')
+  clear1.textContent = '清除按钮1点击事件'
+
+  const btn1OnClick = () => {
+    console.log('按钮1被点了')
+  }
+
+  on(btn1, 'click', btn1OnClick)
+  on(btn1, 'click', btn1OnClick)
+
+  on(clear1, 'click', () => {
+    off(btn1, 'click', btn1OnClick)
+  })
+
+  div.appendChild(btn1)
+  div.appendChild(btn2)
+  div.appendChild(clear1)
+  app.appendChild(div)
+}
+
+{
+  console.log('事件监听')
+  const clear1 = createEl('button')
+  clear1.textContent = '清除委托事件'
+  const div = createEl('div')
+  const ul = createEl('ul')
+  const li1 = createEl('li')
+  const li2 = createEl('li')
+  const li3 = createEl('li')
+  ul.appendChild(li1)
+  ul.appendChild(li2)
+  ul.appendChild(li3)
+  div.appendChild(ul)
+  div.appendChild(clear1)
+  div.classList.add('parent')
+  app.appendChild(div)
+  const clickLi = () => {
+    console.log('li 被委托')
+  }
+  on(div, 'click', 'li', clickLi)
+  on(clear1, 'click', () => {
+    console.log('清除委托事件')
+    off(div, 'click', clickLi)
+  })
+}
+
+// app.innerHTML = `
+//   <h1>Hello Vite!</h1>
+//   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+// `
