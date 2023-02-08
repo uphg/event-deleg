d9a7b1914745fed79c749d1acc024bb90b5979e5
lvheng <2633565885@qq.com>
2023-02-08 22:00:14 +0800

    refactor: 优化 off 方法

--- src/index.ts
+++ src/index.ts
@@ -1,57 +1,58 @@
-const DELEG = Symbol('delegateEvent')
-const LISTEN = Symbol('listenerState')
+const delegateHandler = Symbol('delegateHandler')
 
-interface EventDelegHandler {
+export interface EventDelegHandler {
   (e: Event, ...args: unknown[]): unknown
-  [DELEG]?: EventListenerOrEventListenerObject
-  [LISTEN]?: boolean | null
+  [delegateHandler]?: EventListenerOrEventListenerObject
 }
 
-type EventDelegOptions = boolean | AddEventListenerOptions | undefined
+export type EventDelegOptions = boolean | EventListenerOptions | undefined
+export type EventDelegElement = HTMLElement | Document | Window
 
 export function on(
-  element: HTMLElement | Document | Window,
+  el: EventDelegElement,
   eventName: string,
   selector: string | EventDelegHandler,
   handler?: EventDelegHandler | EventDelegOptions,
   options?: EventDelegOptions
 ) {
-  if (!element || !eventName || !selector) return
-  if (typeof selector === 'function') {
-    selector[LISTEN] = true
-    element.addEventListener(eventName, selector, (handler as EventDelegOptions))
-  } else {
-    const listener =  (e: Event) => {
-      let el = e.target
-      while (!(el as Element)?.matches(selector)) {
-        if (element === el) {
-          el = null
+  if (!el || !eventName || !selector) return
+
+  if (isFunction(selector)) {
+    el.addEventListener(eventName, selector, handler as EventDelegOptions)
+  } else if (isFunction(handler)) {
+    const listener = (event: Event) => {
+      let { target } = event
+      while (!(target as Element)?.matches(selector)) {
+        if (el === target) {
+          target = null
           break
         }
-        el = (el as Node)?.parentNode
+        target = (target as Node)?.parentNode
       }
-      el && (handler as EventDelegHandler).call(el, e, el)
+      target && handler.call(target, event, target)
     }
-    if (handler) {
-      (handler as EventDelegHandler)[DELEG] = listener
-      element.addEventListener(eventName, listener, options)
-    }
+    handler[delegateHandler] = listener
+    el.addEventListener(eventName, listener, options)
   }
-  return element
+
+  return el
 }
 
 export function off(
-  element: HTMLElement | Document | Window,
+  el: EventDelegElement,
   eventName: string,
   handler: EventDelegHandler,
   options?: EventDelegOptions
 ) {
-  if (!element || !eventName || !handler) return
-  if (handler[LISTEN]) {
-    handler[LISTEN] = null
-    element.removeEventListener(eventName, handler, options)
-  }
-  const deleg = handler[DELEG]
-  deleg && element.removeEventListener(eventName, deleg, options)
-  return element
+  if (!el || !eventName || !handler) return
+
+  el.removeEventListener(eventName, handler, options)
+  const deleg = handler[delegateHandler]
+  deleg && el.removeEventListener(eventName, deleg, options)
+
+  return el
+}
+
+function isFunction(value: unknown): value is Function {
+  return typeof value === 'function'
 }
